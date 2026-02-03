de5b826be62b43dfc00947377ab50c361a9341e4
Luis Gordillo <speedforce2426@gmail.com>
2026-02-03 10:10:15 -0500

    Initial commit

---
+++ src/main/java/com/netsolucions/netsolucions_api/NetsolucionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.netsolucions.netsolucions_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetsolucionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetsolucionsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netsolucions/netsolucions_api/NetsolucionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netsolucions.netsolucions_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetsolucionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
